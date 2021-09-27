ad1910dc4bef78113ae76318965fcaccc72422f5
Priscila <pri.damasceno@live.com>
2021-09-27 10:26:48 -0300

    Router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+//São meus componentes
+import { CadastrarComponent } from './cadastrar/cadastrar.component';
+import { EntrarComponent } from './entrar/entrar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'entrar', pathMatch: 'full' },
+  { path: 'entrar', component: EntrarComponent },
+  { path: 'cadastrar', component: CadastrarComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
