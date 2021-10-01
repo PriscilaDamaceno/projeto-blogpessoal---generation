10a164cd170f8abaac82aba274a316f4bcb3a8e0
Priscila <pri.damasceno@live.com>
2021-09-30 23:32:35 -0300

    30-09 botões e afins

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -2,6 +2,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment.prod';
 import { Usuario } from '../model/Usuario';
 import { UsuarioLogin } from '../model/UsuarioLogin'
 
@@ -20,7 +21,13 @@ export class AuthService {
     return this.http.post<Usuario>('https://pridamaceno.herokuapp.com/usuarios/cadastrar', user)
 
   }
-
+logado(){
+  let ok: boolean = false
+if (environment.token != ''){
+  ok = true
+}
+  return ok
+}
 }
 
 //https://pridamaceno.herokuapp.com
